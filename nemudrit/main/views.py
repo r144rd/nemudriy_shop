c8b7e103c8b924b92eb01040d05778001bb73108
artemiy <shumk1nj@mail.ru>
2024-12-20 15:36:21 +0300

    v1.4.1

--- nemudrit/main/views.py
+++ nemudrit/main/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Product , Category 
 
-
-
-
-
 def popular_list(request):
     products = Product.objects.filter(available = True)[:3]
     return render(request,'main/index/index.html',
@@ -17,4 +13,7 @@ def product_detail(request,slug):
     return render(request,
                     'main/product/detail.html',
                     {"product": product})
+
+
+
 
